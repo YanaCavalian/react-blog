bafdfee7f361be19f4167e9a50568bac48155ffd
Yana <cavaljn@icloud.com>
2024-01-15 23:31:28 +0100

    last changes

--- src/pages/Project/Project..jsx
+++ src/pages/Project/Project..jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import './Project.css'
-import CommentInput from '../../components/CommentInput/CommentInput'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./Project.css";
+import CommentInput from "../../components/CommentInput/CommentInput";
 
 export const Project = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div className="project-container">
       <div className="project">
-        <h2>{t('project1Title')}</h2>
-        <p>{t('project1Description')}</p>
+        <h2>{t("project1Title")}</h2>
+        <p>{t("project1Description")}</p>
         <div className="project-links">
           <a
             href="https://github.com/yanacavalian/yogo-website"
@@ -29,8 +29,8 @@ export const Project = () => {
         </div>
       </div>
       <div className="project">
-        <h2>{t('project2Title')}</h2>
-        <p>{t('project2Description')}</p>
+        <h2>{t("project2Title")}</h2>
+        <p>{t("project2Description")}</p>
         <div className="project-links">
           <a
             href="https://github.com/YanaCavalian/Calculator"
@@ -46,13 +46,8 @@ export const Project = () => {
           >
             Demo
           </a>
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="180"
-              width="180"
-              viewBox="0 0 640 512"
-            >
+          <div className="svg-block">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
               <path d="M64 96c0-35.3 28.7-64 64-64H512c35.3 0 64 28.7 64 64V352H512V96H128V352H64V96zM0 403.2C0 392.6 8.6 384 19.2 384H620.8c10.6 0 19.2 8.6 19.2 19.2c0 42.4-34.4 76.8-76.8 76.8H76.8C34.4 480 0 445.6 0 403.2zM281 209l-31 31 31 31c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-48-48c-9.4-9.4-9.4-24.6 0-33.9l48-48c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9zM393 175l48 48c9.4 9.4 9.4 24.6 0 33.9l-48 48c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l31-31-31-31c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z" />
             </svg>
           </div>
@@ -60,5 +55,5 @@ export const Project = () => {
       </div>
       <CommentInput />
     </div>
-  )
-}
+  );
+};
